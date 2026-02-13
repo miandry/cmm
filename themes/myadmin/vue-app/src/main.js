9d46672c8120c122b472c6810e313df3b29b095f
tokyNomenjanahary <razananirianatoky@gmail.com>
2026-02-13 11:15:51 +0300

    fix route redirect

--- themes/myadmin/vue-app/src/main.js
+++ themes/myadmin/vue-app/src/main.js
@@ -66,27 +66,15 @@ const router = createRouter({
   routes,
 });
 
-// router.beforeEach((to, from, next) => {
-//   const requiredRoles = to.meta.roles;
-
-//   // Route publique
-//   if (!requiredRoles) {
-//     return next();
-//   }
-
-//   // Pas connecté
-//   if (!window.APP_DATA?.isLoggedIn) {
-//     return next("/");
-//   }
-
-//   // Vérification des rôles
-//   if (!hasAnyRole(requiredRoles)) {
-//     console.warn("Accès refusé", to.path);
-//     return next("/"); // ou page 403
-//   }
-
-//   next();
-// });
+// Rediriger vers caisse si la route n'existe pas
+router.beforeEach((to, from, next) => {
+  if (to.matched.length === 0) {
+    // Route non trouvée
+    next('/caisse');
+  } else {
+    next();
+  }
+});
 
 const pinia = createPinia();
 
