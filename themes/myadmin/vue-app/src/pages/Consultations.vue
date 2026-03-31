8a61f45b2954c4e3cce8f0758627e419446524a9
tokyNomenjanahary <razananirianatoky@gmail.com>
2026-03-31 09:47:22 +0300

    consultation fix

--- themes/myadmin/vue-app/src/pages/Consultations.vue
+++ themes/myadmin/vue-app/src/pages/Consultations.vue
@@ -514,8 +514,6 @@ export default {
                     offset: 1
                 };
                 await userStore.fetchUsers(doctorQueryOptions);
-                alert('vfghjkl')
-                console.log(userStore.users);
             }
 
             // reset form si c'est add
